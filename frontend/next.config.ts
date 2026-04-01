6050ec25baca670303b53117f19b1cc61c753921
meharkp7 <meharkapoor7@gmail.com>
2026-04-01 23:43:22 +0530

    gitignore updates

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://weavenet.onrender.com';
     return [
       {
         source: '/api/:path*',
